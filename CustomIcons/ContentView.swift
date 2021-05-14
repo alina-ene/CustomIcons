c5e3d77f6a01b62a1c624cd92bf87f6943589e92
Alina <alina.c.ene@gmail.com>
2021-05-14 12:33:12 +0100

    Initial Commit

---
+++ CustomIcons/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CustomIcons
+//
+//  Created by Alina Ene on 14/05/2021.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ CustomIcons/CustomIconsApp.swift
@@ -0,0 +1,17 @@
+//
+//  CustomIconsApp.swift
+//  CustomIcons
+//
+//  Created by Alina Ene on 14/05/2021.
+//
+
+import SwiftUI
+
+@main
+struct CustomIconsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
