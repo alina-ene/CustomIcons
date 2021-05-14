ddd553a26ac4fff22556a1e39eb515a4c4bfd0c6
Alina <alina.c.ene@gmail.com>
2021-05-14 17:09:09 +0100

    Updates Tests files

--- CustomIconsTests/CustomIconsTests.swift
+++ CustomIconsTests/CustomIconsTests.swift
@@ -10,16 +10,8 @@ import XCTest
 
 class CustomIconsTests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
     func testLocalJson() throws {
-        let iconsService = IconsService()
+        let iconsService = IconsService(networkService: NetworkService())
         iconsService.fetchLocalIcons()
         assert(!iconsService.icons.isEmpty, "icons list should not be empty")
         assert(iconsService.icons.count == 16, "icons list should hold 16 elements")
@@ -27,13 +19,6 @@ class CustomIconsTests: XCTestCase {
         assert(iconsService.icons[0].title == "Lime Green")
         assert(iconsService.icons[0].subtitle == "#A5EA9B")
         assert(iconsService.icons[0].image == "https://irapps.github.io/wzpsolutions/tests/ios-custom-icons/LimeGreen.png")
-    }
-
-    func testPerformanceExample() throws {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
-        }
     }
 
 }
--- CustomIconsUITests/CustomIconsUITests.swift
+++ CustomIconsUITests/CustomIconsUITests.swift
@@ -9,34 +9,13 @@ import XCTest
 
 class CustomIconsUITests: XCTestCase {
 
-    override func setUpWithError() throws {
-        // Put setup code here. This method is called before the invocation of each test method in the class.
-
-        // In UI tests it is usually best to stop immediately when a failure occurs.
-        continueAfterFailure = false
-
-        // In UI tests it’s important to set the initial state - such as interface orientation - required for your tests before they run. The setUp method is a good place to do this.
-    }
-
-    override func tearDownWithError() throws {
-        // Put teardown code here. This method is called after the invocation of each test method in the class.
-    }
-
-    func testLocalJson() throws {
+    func testLoadedData() throws {
         // UI tests must launch the application that they test.
         let app = XCUIApplication()
         app.launch()
         XCTAssertTrue(app.staticTexts["Lime Green"].exists)
         XCTAssertTrue(app.staticTexts["#A5EA9B"].exists)
-        XCTAssertTrue(app.otherElements["https://irapps.github.io/wzpsolutions/tests/ios-custom-icons/LimeGreen.png"].exists)
+        XCTAssertTrue(app.images["https://irapps.github.io/wzpsolutions/tests/ios-custom-icons/LimeGreen.png"].exists)
     }
 
-    func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
-            // This measures how long it takes to launch your application.
-            measure(metrics: [XCTApplicationLaunchMetric()]) {
-                XCUIApplication().launch()
-            }
-        }
-    }
 }
